5f17383a55b69e37b6ed883e623e25175b556976
Thais <thaisplaneta2017@gmail.com>
2023-05-30 11:32:50 -0300

    adicionei uma div para vir outra imagem

--- src/componentes/Imagens/Imagem.js
+++ src/componentes/Imagens/Imagem.js
@@ -4,10 +4,17 @@ function Imagem(props){
     
     console.log(props.item)
     return ( 
+    <>
     <div className="box-pagina-principal media1" onClick={props.item}>
     <img src={props.link} alt=""></img>
     <h4>{props.titulo}</h4>
     </div>
+
+    <div className="box-pagina-principal media2" onClick={props.item}>
+    <img src={props.link2} alt=""></img>
+    <h4>{props.titulo2}</h4>
+    </div>
+    </>
     );
 }
 
